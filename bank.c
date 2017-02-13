d02bf12a052246b0fe2be4c6b4d0bc48023bdf5e
Гак Денис Романович <is641s08@lwp02.csc.local>
2017-02-13 09:37:47 +0700

    add month

--- bank.c
+++ bank.c
@@ -3,7 +3,7 @@
 int main()
 {
 
-int day,money,result,check;
+int day,money,result,check,month;
 printf("Enter date and money: ");
 check=scanf("%d %d",&day,&money);
 if((check)&&(day>=0)&&(day<=365)&&(money>=10000))
@@ -12,13 +12,21 @@ if((check)&&(day>=0)&&(day<=365)&&(money>=10000))
 		result=money*0.9;}
 	else
 	{
-		if(day<121){
-			if(money<100000){
-				result=money*1.02;
-				}
+		if(day<121){31 59 90 120
+			if(day>31){
+				month=1;}
 			else
-			{
-				result=money*1.03;
+				if(day>59){month=2;}
+				else
+				if(day>90){month=3;}
+			if(money<100000){
+				for(month;month!=0;month--){
+				result=money*1.02;}
+				}
+				
+			else
+			{	for(month;month!=0;month--){
+				result=money*1.03;}
 				}
 			}
 		else
